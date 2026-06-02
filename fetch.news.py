caf9f5eb25ebd703a2fa882b872f56b31fde6f81
ashlink2004-max <ashlink2004@gmail.com>
2026-06-02 13:29:23 +0530

    Convert Published Timestamp to Date

--- fetch.news.py
+++ fetch.news.py
@@ -31,3 +31,8 @@ for article in articles:
     title = article.get("title")
     source_name = article.get("source", {}).get("name")
     published_at = article.get("publishedAt")
+
+    news_date = datetime.strptime(
+    published_at,
+    "%Y-%m-%dT%H:%M:%SZ"
+).date()
