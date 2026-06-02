41eb4c42253ccf4766578782e2d74da01e08eb11
ashlink2004-max <ashlink2004@gmail.com>
2026-06-02 13:31:23 +0530

    Add Sentiment Classification Logic

--- fetch.news.py
+++ fetch.news.py
@@ -39,3 +39,9 @@ for article in articles:
     
 sentiment_score = TextBlob(title).sentiment.polarity
 
+if sentiment_score > 0:
+    sentiment_label = "Positive"
+elif sentiment_score < 0:
+    sentiment_label = "Negative"
+else:
+    sentiment_label = "Neutral"
