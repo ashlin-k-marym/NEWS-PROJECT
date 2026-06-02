c8db4e2cac990ffa0e1847dc3d1bf3f9adf8744e
ashlink2004-max <ashlink2004@gmail.com>
2026-06-02 13:13:29 +0530

    Add News Data Table Display

--- dashboard.py
+++ dashboard.py
@@ -50,3 +50,16 @@ if not df.empty:
     
     if df.empty:
     st.warning("No news data found in database.")
+
+display_df = df[
+    [
+        "news_date",
+        "source_name",
+        "title",
+        "sentiment_score",
+        "sentiment_label",
+        "created_at"
+    ]
+]
+
+st.dataframe(display_df)
