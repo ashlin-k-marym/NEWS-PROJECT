d8aa3fbe8fdb109ca06ab3f4842323254c3c8489
ashlink2004-max <ashlink2004@gmail.com>
2026-06-04 11:24:47 +0530

    Added config lambda module

---
+++ Modules/Lambda_Module/config.py
@@ -0,0 +1,11 @@
+# DATABASE CONFIGURATION
+
+DB_HOST = "database-1.ctmgqmi4uiy0.ap-south-1.rds.amazonaws.com"
+DB_NAME = "new_db"
+DB_USER = "postgres"
+DB_PASSWORD = "ashlinkkunjippadavil12345"
+DB_PORT = 5432
+
+# NEWS API
+
+NEWS_API_KEY = "9937956bd25b4d39a610bafcaa761aa4"
